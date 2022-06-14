403baec4eeff9012146d477a6b9fed2a47ac4651
Nico Wagner <info@ni-wa.de>
2022-06-14 08:05:59 +0200

    added Typescript-typings

---
+++ types/types.d.ts
@@ -0,0 +1,91 @@
+export declare class SenecIES {
+    private readonly options;
+    private readonly DataValidationMaxValues;
+    private readonly DataValidationOffset;
+    private DataValidationCalcLS;
+    private SENECDataValidation;
+
+    constructor(host: string);
+
+    handleSenec(): Promise<{
+        responseSchema: EnergyValues;
+        validPacket: boolean;
+    }>;
+}
+
+
+/**
+ * @author Jens H.
+ */
+export declare class DataValidation {
+    private readonly n;
+    private readonly t;
+    private params;
+    private xData;
+    private yData;
+
+    /**
+     * @param maxValues - Max Values used for calculation
+     * @param t Max difference between calculation and value to test
+     */
+    constructor(maxValues: number, t: number);
+
+    private static cov;
+    private calcLS;
+
+    addValue(value: number, calcLS?: boolean): void;
+
+    private predicted;
+
+    checkValue(value: number): number;
+}
+
+
+export declare class DataParser {
+    static parseXML(xml: string): ResponseSchema;
+
+    static parseData(xml: ResponseSchema): null | EnergyValues;
+}
+
+
+export interface ResponseSchema {
+    body: {
+        version: {
+            _text: string;
+        };
+        client: {
+            _text: string;
+        };
+        client_ver: {
+            _text: string;
+        };
+        item_list_size: [{}, {
+            _text: string;
+        }];
+        item_list: {
+            i: {
+                n: {
+                    _text: string;
+                };
+                v: {
+                    _text: string;
+                };
+            }[];
+        };
+    };
+}
+
+
+export interface EnergyValues {
+    Energy: EnergyValuesPlain<number>;
+    Power: EnergyValuesPlain<number>;
+}
+
+export interface EnergyValuesPlain<T> {
+    BatteryCharge: T;
+    BatteryDischarge: T;
+    GridExport: T;
+    GridImport: T;
+    HouseConsumption: T;
+    PVGenerated: T;
+}
